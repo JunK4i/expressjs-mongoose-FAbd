8dc8e35e48d5605332e5a9baf5428d8fd1ee7979
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2023-09-26 15:43:01 +0800

    Update whitelist.ts

--- src/configs/whitelist.ts
+++ src/configs/whitelist.ts
@@ -1,6 +1,6 @@
 const whitelist = [
     "http://localhost:3000",
-    "http://localhost:3333",
+    "http://localhost:5173",
     "https://react-frontend-production-9531.up.railway.app/",
 ]
 export default whitelist;
